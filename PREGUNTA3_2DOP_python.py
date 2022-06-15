1a59284ded86e2b50351e51064ff7f352d4f5d6c
Keitling <105679193+Keitling@users.noreply.github.com>
2022-06-14 23:37:57 -0400

    Add files via upload

---
+++ PREGUNTA3_2DOP_python.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 14 23:33:56 2022
+
+@author: Flores Salinas
+"""
+
+def rec_fib(n):
+    if n > 1:
+        return rec_fib(n-1) + rec_fib(n-2)
+    return n
+for i in range(10):
+    print(rec_fib(i))
