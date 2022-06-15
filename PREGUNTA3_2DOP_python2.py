5a598e5dd62fa21076c8a852626bd6058ca0891b
Keitling <105679193+Keitling@users.noreply.github.com>
2022-06-14 23:37:47 -0400

    Add files via upload

---
+++ PREGUNTA3_2DOP_python2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 14 23:34:45 2022
+
+@author: Keitling Salinas
+"""
+
+def fibonacci(num):
+    arr = [0,1]
+    if num==1:
+        print('0')
+    elif num==2:
+        print('[0,','1]')
+    else:
+        while(len(arr)<num):
+            arr.append(0)
+        if(num==0 or num==1):
+            return 1
+        else:
+            arr[0]=0
+            arr[1]=1
+            for i in range(2,num):
+                arr[i]=arr[i-1]+arr[i-2]
+            print(arr)
+
+fibonacci(10)
